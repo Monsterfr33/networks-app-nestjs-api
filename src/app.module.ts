7c1e15b2834f26740f2ef084d80c18fbdca643a2
Muhammad Awais <awaisshaikh94@gmail.com>
2021-09-19 16:35:09 +0500

    hot-fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import config from './config/keys';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
 import { InterestHobbiesModule } from './modules/interest-hobbies/interest-hobbies.module';
 import { ProfessionalAboutModule } from './modules/professional-about/professional-about.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(config.mongoURI),
+    AuthModule,
     InterestHobbiesModule,
     ProfessionalAboutModule
   ],
